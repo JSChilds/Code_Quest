147e7994e43be60f9070633553ec6228ca155f5a
Tech-A40 <Tech-A40@Tech-A40s-MacBook-Air.local>
2017-06-23 12:28:47 +0100

    Made first version of instructions page

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(function() {
+	$input = $('input');
+	$button = $('button');
+
+	$button.click(function(event){
+		console.log('hey');
+
+
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// ENTER FORM SUBMIT SYNTAX FOR LATER
+	// var choice = '';
+
+	// // $input.keydown(function(event) {
+ // //    	if (event.keyCode == 13) {
+ // //    		choice = $(this).val();
+	// // 	    if (choice != '') {
+	// // 	      	console.log(choice);
+	// // 	      	$input.val('');
+	// // 	    }
+	// // 	}
+	// //     if (choice.toUpperCase() == 'PLAY') {
+ // //  			console.log('p');
+ // //  		}
+ // //  		else if (choice.toUpperCase() == 'INSTRUCTIONS') {
+ // //  		console.log('i');
+ // //  		}
+ // //  	});
+
+
+});
